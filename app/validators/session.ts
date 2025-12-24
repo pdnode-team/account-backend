1b757abf7b1915ee92974cfd8014433a3877e3d3
Bret <Bretren2013@gmail.com>
2025-12-23 17:24:24 -0800

    fix: Fix overly loose character ranges in regular expressions

--- app/validators/session.ts
+++ app/validators/session.ts
@@ -2,7 +2,7 @@ import vine from "@vinejs/vine";
 
 export const createSessionValidator = vine.compile(
     vine.object({
-      username: vine.string().trim().regex(/^[a-zA-Z0-9.-_]+$/).minLength(3).maxLength(12).optional(),
+      username: vine.string().trim().regex(/^[a-zA-Z0-9._-]+$/).minLength(3).maxLength(12).optional(),
       email: vine.string().trim().email().optional(),
       password: vine.string().trim(),
       rememberMe: vine.boolean().optional(),
