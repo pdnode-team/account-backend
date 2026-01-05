1823dbe5c687bb2ee9bbea0c58eb52297da285b8
Bret <Bretren2013@gmail.com>
2026-01-04 21:55:05 -0800

    Added:  - mail.ts: Resend change to SMTP and added more env var  - sessions_controller.ts: Added Cross-domain shared access key (Important information: Not tested.)

--- config/mail.ts
+++ config/mail.ts
@@ -2,7 +2,7 @@ import env from "#start/env";
 import { defineConfig, transports } from "@adonisjs/mail";
 
 const mailConfig = defineConfig({
-  default: "resend",
+  default: "smtp",
 
   /**
    * The mailers object can be used to configure multiple mailers
@@ -10,13 +10,13 @@ const mailConfig = defineConfig({
    * options.
    */
   from: {
-    address: "account@mail.pdnode.com",
-    name: "Pdnode Account",
+    address: env.get("SMTP_ADDRESS"),
+    name: env.get("SMTP_NAME"),
   },
   mailers: {
-    resend: transports.resend({
-      key: env.get("RESEND_API_KEY"),
-      baseUrl: "https://api.resend.com",
+    smtp: transports.smtp({
+      host: env.get('SMTP_HOST') ?? "localhost",
+      port: env.get('SMTP_PORT'),
     }),
   },
 });
